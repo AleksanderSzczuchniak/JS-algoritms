96d9ad60e41c1e3d4a90082221160dd70e05033a
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-12-14 14:40:37 +0100

    logic

--- script.js
+++ script.js
@@ -1,101 +1,62 @@
-const average = arr => arr.reduce(
-    (r, e, i, arr) => r + (e / arr.length),
-    0
-  )
-  
-  if (average([]) !== 0) throw Error('Average not works!')
-  if (average([1, 2, 3, 4]) !== 2.5) throw Error('Average not works!')
-  
-  const median = arr => {
-    const length = arr.length
-    const sorted = arr.sort()
-  
-    if (length === 0) return 0
-  
-    if (length % 2 !== 0) {
-      return sorted[Math.floor(length / 2)]
-    } else {
-      return (sorted[length / 2 - 1] + sorted[length / 2]) / 2
-    }
-  }
-  
-  if (median([1, 2, 3, 4]) !== 2.5) throw Error('Median not works!')
-  if (median([1, 2, 4, 3]) !== 2.5) throw Error('Median not works!')
-  if (median([1, 2, 3]) !== 2) throw Error('Median not works!')
-  if (median([]) !== 0) throw Error('Median not works!')
-  
-  const min = arr => {
-    if (arr.length === 0) return 0
-  
-    return Math.min.apply(null, arr)
-  }
-  
-  if (min([1, 2, 3, 4]) !== 1) throw Error('Min not works!')
-  if (min([-1, 1, -3]) !== -3) throw Error('Min not works!')
-  if (min([]) !== 0) throw Error('Min not works!')
-  
-  const max = arr => {
-    if (arr.length === 0) return 0
-  
-    return Math.max(...arr)
-  }
-  
-  if (max([1, 2, 3, 4]) !== 4) throw Error('Max not works!')
-  if (max([-1, 1, -3]) !== 1) throw Error('Max not works!')
-  if (max([]) !== 0) throw Error('Max not works!')
-  
-  const getStats = arr => ({
-    min: min(arr),
-    max: max(arr),
-    median: median(arr),
-    avg: average(arr)
-  })
-  
-  if (
-    JSON.stringify(getStats([1, 2, 4, 3]))
-    !==
-    JSON.stringify({ min: 1, max: 4, median: 2.5, avg: 2.5 })
-  ) throw Error('Stats not works!')
-  
-  const getValueFromInput = input => (
-    input.value.split(' ')
-      .filter(number => number !== '')
-      .map(number => Number(number))
-      .filter(number => number || number === 0)
-  )
-  
-  if (
-    JSON.stringify(getValueFromInput({ value: '1 2 34 5' }))
-    !==
-    JSON.stringify([1, 2, 34, 5])
-  ) throw Error('Get value form input not works!')
-  if (
-    JSON.stringify(getValueFromInput({ value: '1      2      34 5' }))
-    !==
-    JSON.stringify([1, 2, 34, 5])
-  ) throw Error('Get value form input not works!')
-  if (
-    JSON.stringify(getValueFromInput({ value: '1 a ala ola 2' }))
-    !==
-    JSON.stringify([1, 2])
-  ) throw Error('Get value form input not works!')
-  
-  const buttonClickHandler = () => {
+import { getStats, getValueFromInput } from './logic.js'
+
+export default function App(selector) {
+    this.container = document.querySelector(selector || 'body')
+    this.inputElement = null
+    this.buttonElement = null
+    this.paragraphElement = null
+
+    this.resultText = ''
+
+    this.init()
+}
+
+App.prototype.init = function () {
+    this.render()
+}
+
+App.prototype.render = function () {
+    this.container.innerHTML = ''
+
+    this.inputElement = document.createElement('input')
+    this.buttonElement = document.createElement('button')
+    this.paragraphElement = document.createElement('p')
+
+    this.inputElement.classList.add('input')
+    this.buttonElement.classList.add('button')
+    this.paragraphElement.classList.add('result')
+
+    this.buttonElement.innerText = 'Click'
+
+    this.paragraphElement.innerHTML = this.resultText
+
+    this.addEventsListeners()
+
+    this.container.appendChild(this.inputElement)
+    this.container.appendChild(this.buttonElement)
+    this.container.appendChild(this.paragraphElement)
+}
+
+App.prototype.buttonClickHandler = function () {
     const stats = getStats(
-      getValueFromInput(
-        document.querySelector('.input')
-      )
+        getValueFromInput(
+            this.inputElement
+        )
     )
-  
-    document.querySelector('.result').innerHTML = `
+
+    this.resultText = `
         <p>MIN: ${stats.min}</p>
         <p>MAX: ${stats.max}</p>
         <p>AVG: ${stats.avg}</p>
         <p>MED: ${stats.median}</p>
     `
-  }
-  
-  document.querySelector('.button').addEventListener(
-    'click',
-    buttonClickHandler
-  )
+
+    this.render()
+}
+
+App.prototype.addEventsListeners = function () {
+    this.buttonElement.addEventListener(
+        'click',
+        this.buttonClickHandler.bind(this)
+    )
+}
